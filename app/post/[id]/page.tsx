6b1d0fc3ffbee8c79899bda9b13a807e0c4e1fb9
maggik2048 <maggik2048@gmail.com>
2026-03-31 02:17:36 -0700

    [update] detailed page is created 상세 페이지 만들기
    app/post/[id]/page.tsx

---
+++ app/post/[id]/page.tsx
@@ -0,0 +1,30 @@
+import { supabase } from "@/lib/supabase";
+import ReactMarkdown from "react-markdown";
+import remarkMath from "remark-math";
+import rehypeKatex from "rehype-katex";
+import "katex/dist/katex.min.css";
+
+export default async function PostPage(props: any) {
+  const params = await props.params;
+
+  const { data, error } = await supabase
+    .from("posts")
+    .select("*")
+    .eq("id", params.id)
+    .single();
+
+  if (error) return <div>에러: {error.message}</div>;
+
+  return (
+    <div style={{ padding: 40 }}>
+      <h1 style={{ fontSize: 32 }}>{data.title}</h1>
+
+      <ReactMarkdown
+        remarkPlugins={[remarkMath]}
+        rehypePlugins={[rehypeKatex]}
+      >
+        {data.content}
+      </ReactMarkdown>
+    </div>
+  );
+}
