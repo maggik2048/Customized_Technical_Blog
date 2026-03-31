a1cd5d89f7685dc0cb9ddc640092a8c4a674b962
maggik2048 <maggik2048@gmail.com>
2026-03-31 02:15:20 -0700

    [update] postTitle 에 link를 적용하여 detailed post page로갈수있도록 기반생성
    [update] postTitle 에 link를 적용하여 detailed post page로갈수있도록 기반생성. 제목에링크적용

--- app/category/[slug]/page.tsx
+++ app/category/[slug]/page.tsx
@@ -3,6 +3,7 @@ import ReactMarkdown from "react-markdown";
 import remarkMath from "remark-math";
 import rehypeKatex from "rehype-katex";
 import "katex/dist/katex.min.css";
+import Link from "next/link";
 
 export default async function CategoryPage(props: any) {
   const params = await props.params;
@@ -25,8 +26,14 @@ export default async function CategoryPage(props: any) {
 
       {data?.map((post) => (
         <div key={post.id} style={{ marginBottom: 30 }}>
-          <h2>{post.title}</h2>
+          {/* 제목에 링크 적용 */}
+          <Link href={`/post/${post.id}`}>
+            <h2 style={{ cursor: "pointer", color: "skyblue" }}>
+              {post.title}
+            </h2>
+          </Link>
 
+          {/* 마크다운 렌더링 */}
           <ReactMarkdown
             remarkPlugins={[remarkMath]}
             rehypePlugins={[rehypeKatex]}
