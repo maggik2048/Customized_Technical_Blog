f64d1aaa63447a891e036ff005e1373896c3ae10
maggik2048 <maggik2048@gmail.com>
2026-03-30 21:49:40 -0700

    [update] Categorybox shadow color Change to White

--- app/components/CategoryCard.tsx
+++ app/components/CategoryCard.tsx
@@ -22,7 +22,7 @@ export default function CategoryCard({ item }: { item: Item }) {
           transition
           p-6
           rounded-lg
-          shadow-[0_0_20px_rgba(0,0,0,0.5)]
+          shadow-[0_0_20px_rgba(255,255,255,0.2)]
         "
       >
         <h2
